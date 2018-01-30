302c6e0674655bad37e10e92c073deeb8a5be7cf
treyshel <tshelton@basecampcodingacademy.org>
2018-01-30 09:33:40 -0600

    i've made my gas class and my paybefore and payafter objects

--- GasPump.java
+++ GasPump.java
@@ -1,5 +1,37 @@
 package com.company;
 
 public class GasPump {
-    
+    public String typeOFgas;
+    public Integer amountOFgallons;
+    public Integer priceOFgas;
+
+    GasPump(String gasType, Integer gallonAmount, Integer gasPrice) {
+        typeOFgas = gasType;
+        amountOFgallons = gallonAmount;
+        priceOFgas = gasPrice;
+    }
+
+    public double prepay(double money) {
+        double gallons = 0.0;
+        if (typeOFgas.equals("Regular")) {
+            return money / 2.09;
+        } else if (typeOFgas.equals("Mid-Grade")) {
+            return money / 2.19;
+        } else if (typeOFgas.equals("Premium")) {
+            return money / 2.29;
+        } return gallons;
+    }
+
+    public double payafter(double gallons) {
+        double money = 0.0;
+        if (typeOFgas.equals("Regular")) {
+            return gallons * 2.09;
+        } else if (typeOFgas.equals("Mid-Grade")) {
+            return gallons * 2.19;
+        } else if (typeOFgas.equals("Premium")) {
+            return gallons * 2.29;
+        } return money;
+    }
+
+
 }
--- Main.java
+++ Main.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 
 public class Main {
 
+    public String getType(String gas) {
+        String type = "";
+        if (gas.equals(87)) {
+            return "Regular";
+        } else if (gas.equals(89)) {
+            return "Mid-Grade";
+        } else if (gas.equals(92)) {
+            return "Premium";
+        } return type;
+    }
+
+
     public static void main(String[] args) {
 
         String payBeforeOrAfter;
@@ -10,9 +22,14 @@ public class Main {
         System.out.println("\t\tWelcome to Trey's Place!\n1 -> Pay Before\n2 -> Pay After");
         payBeforeOrAfter = beforeOrAfterInput.next();
 
-        String gastype;
-        Scanner gastypeInput = new Scanner(System.in);
-        System.out.println("What type of gas?\n1 -> Regular ($2.09)\n2 -> Mid-Grade ($2.19)\n3 -> Premium ($2.29");
-        gastype = gastypeInput.next();
+        if (payBeforeOrAfter.equals("1")) {
+            String gastype;
+            Scanner gastypeInput = new Scanner(System.in);
+            System.out.println("What type of gas?\n87 -> Regular ($2.09)\n89 -> Mid-Grade ($2.19)\n92 -> Premium ($2.29)");
+            gastype = gastypeInput.next();
+        } else {
+            System.out.println("NOPE");
         }
+    }
 }
+
